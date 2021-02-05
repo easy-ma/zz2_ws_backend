3b82f04b1293758e7c0931809c029fbba6732333
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-05 14:28:56 +0100

    feat: swagger

--- turradgiver-api/Startup.cs
+++ turradgiver-api/Startup.cs
@@ -26,6 +26,7 @@ namespace turradgiver_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +38,22 @@ namespace turradgiver_api
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
