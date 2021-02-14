b41638fe2eff3696f8c849ce3cde5b4fd218038f
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-14 21:39:51 +0100

    fix: remove all unused using statements

--- data-access/Models/Test.cs
+++ data-access/Models/Test.cs
@@ -1,7 +1,4 @@
-using System;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace DAL.Models
 {
--- turradgiver-api/Startup.cs
+++ turradgiver-api/Startup.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using DAL;
 using DAL.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
