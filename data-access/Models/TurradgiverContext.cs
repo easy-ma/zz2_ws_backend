e048a243aa19a41781161783150d23d983603860
Babidiii <bilal.guillaumie@etu.uca.fr>
2021-02-17 22:34:23 +0100

    fix: migration to net5.0, error user table does not exist --> probably need a migration init ?

--- data-access/Models/TurradgiverContext.cs
+++ data-access/Models/TurradgiverContext.cs
@@ -13,6 +13,8 @@ namespace DAL.Models
             : base(options)
         {
         }
+
+         public virtual DbSet<User> Users { get; set; }
        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -21,7 +23,7 @@ namespace DAL.Models
                 optionsBuilder.UseNpgsql("Server = kandula.db.elephantsql.com; Port = 5432; Database = vktoegob; User Id = vktoegob; Password = sCropnv82b1-7h4jNW3WC96AZNI9ub3d");
             }
         }
-        
+
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
